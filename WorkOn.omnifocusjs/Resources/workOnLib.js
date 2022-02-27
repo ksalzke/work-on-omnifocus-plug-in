6139b27a5bf4a0688d275bc9bad3f984b4007a49
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-28 10:23:53 +1100

    add 'defer until tomorrow' option

--- WorkOn.omnifocusjs/Resources/workOnLib.js
+++ WorkOn.omnifocusjs/Resources/workOnLib.js
@@ -14,7 +14,7 @@
 
     // NB using form as alert not being processed correctly
     const form = new Form()
-    const options = ['Continue work later', 'Mark as complete']
+    const options = ['Continue work later', 'Defer until tomorrow', 'Mark as complete']
 
     // if scheduling plug-in is also installed, add option to schedule for tomorrow or a future date
     const schedulingPlugin = PlugIn.find('com.KaitlinSalzke.Scheduling')
@@ -30,6 +30,9 @@
     for (const nextAction of form.values.nextActions) {
       switch (nextAction) {
         case 'Continue work later':
+          break
+        case 'Defer until tomorrow':
+          task.deferDate = Calendar.current.startOfDay(tomorrow)
           break
         case 'Schedule work tomorrow':
           await schedulingPlugin.library('schedulingLib').rescheduleTask(task, tomorrow)
