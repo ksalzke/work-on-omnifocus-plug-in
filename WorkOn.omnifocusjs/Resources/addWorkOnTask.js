fa2a2d7a350a305889f89d554d2a557a1f1c84cb
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-27 11:21:09 +1100

    extend to work on tasks (not just projects)

--- WorkOn.omnifocusjs/Resources/addWorkOnTask.js
+++ WorkOn.omnifocusjs/Resources/addWorkOnTask.js
@@ -1,19 +1,20 @@
 /* global PlugIn */
 (() => {
   const action = new PlugIn.Action(async function (selection, sender) {
-   
-    selection.projects.forEach(project => {
-      
-      const trimmedName = project.name.match(/\w.*\w/g)[0]
 
-      const newTask = new Task(`Work on: ${trimmedName}`, project.beginning)
+    const selected = Array.from(selection.tasks).concat(Array.from(selection.projects).map(p => p.task))
+   
+    for (const task of selected) {
+      const trimmedName = task.name.match(/\w.*\w/g)[0]
+
+      const newTask = new Task(`Work on: ${trimmedName}`, task.beginning)
       newTask.repetitionRule = new Task.RepetitionRule('FREQ=DAILY', Task.RepetitionMethod.DeferUntilDate)
-    })
+    }
 
   })
 
   action.validate = function (selection, sender) {
-    return selection.projects.length > 0 
+    return selection.tasks.length > 0 || selection.projects.length > 0
   }
 
   return action
