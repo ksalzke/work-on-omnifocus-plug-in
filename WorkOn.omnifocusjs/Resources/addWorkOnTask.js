d1cbed1fdd70023f0f8d74ca122e671fa09e85df
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-27 11:23:32 +1100

    move processing to lib

--- WorkOn.omnifocusjs/Resources/addWorkOnTask.js
+++ WorkOn.omnifocusjs/Resources/addWorkOnTask.js
@@ -1,16 +1,8 @@
 /* global PlugIn */
 (() => {
   const action = new PlugIn.Action(async function (selection, sender) {
-
     const selected = Array.from(selection.tasks).concat(Array.from(selection.projects).map(p => p.task))
-   
-    for (const task of selected) {
-      const trimmedName = task.name.match(/\w.*\w/g)[0]
-
-      const newTask = new Task(`Work on: ${trimmedName}`, task.beginning)
-      newTask.repetitionRule = new Task.RepetitionRule('FREQ=DAILY', Task.RepetitionMethod.DeferUntilDate)
-    }
-
+    for (const task of selected) this.workOnLib.addWorkOnTask(task)
   })
 
   action.validate = function (selection, sender) {
