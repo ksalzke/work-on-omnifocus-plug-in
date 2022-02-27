50d3c6672d31f9c651e203034831e0f9eb4dcdcd
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-27 11:52:55 +1100

    add option to continue work tomorrow if 'Scheduling' plug-in is installed

--- WorkOn.omnifocusjs/Resources/workOnLib.js
+++ WorkOn.omnifocusjs/Resources/workOnLib.js
@@ -15,12 +15,22 @@
     // NB using form as alert not being processed correctly
     const form = new Form()
     const options = ['Continue work later', 'Mark as complete']
+
+    // if scheduling plug-in is also installed, add option to schedule for tomorrow or a future date
+    const schedulingPlugin = PlugIn.find('com.KaitlinSalzke.Scheduling')
+    if (schedulingPlugin !== null) options.splice(1, 0, 'Continue work tomorrow')
+
     form.addField(new Form.Field.Option('nextAction', 'Next action', options, null, options[0]))
 
     await form.show(`You worked on '${trimmedName}'.\nWhat do you want to do next?`, 'OK')
 
     switch (form.values.nextAction) {
       case 'Continue work later':
+        break
+      case 'Continue work tomorrow':
+        const tomorrow = new Date()
+        tomorrow.setDate(tomorrow.getDate() + 1)
+        await schedulingPlugin.library('schedulingLib').rescheduleTask(task, tomorrow)
         break
       case 'Mark as complete':
         task.parent.markComplete()
