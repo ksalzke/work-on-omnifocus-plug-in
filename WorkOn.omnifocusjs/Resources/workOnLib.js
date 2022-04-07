572267b2c73771f6bc6bf7c9067d79fc517a5ef3
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-04-07 09:42:09 +0930

    fix: :bug: fix bug where non-word characters (e.g. parentheses) at end of name were dropped, closes #3

--- WorkOn.omnifocusjs/Resources/workOnLib.js
+++ WorkOn.omnifocusjs/Resources/workOnLib.js
@@ -3,7 +3,7 @@
   const workOnLib = new PlugIn.Library(new Version('1.0'))
 
   workOnLib.addWorkOnTask = (task) => {
-    const trimmedName = task.name.match(/\w.*\w/g)[0]
+    const trimmedName = task.name.match(/\S.*\S/g)[0]
 
     const newTask = new Task(`Work on: ${trimmedName}`, task.beginning)
     newTask.repetitionRule = new Task.RepetitionRule('FREQ=HOURLY', Task.RepetitionMethod.DeferUntilDate)
